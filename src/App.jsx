32a51a21a73e45b0562ab9e7c43553f292a319c1
Laura McGowan <prime.lmcg@gmail.com>
2023-08-09 12:07:45 -0500

    HomePage, Product and Pricing Routh paths created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import HomePage from "./pages/HomePage";
+
 function App() {
-  return <div> WorldWise</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;

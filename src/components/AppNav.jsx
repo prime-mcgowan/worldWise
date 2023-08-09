357ab880c35351ca4728e474185364748b8d0540
Laura McGowan <prime.lmcg@gmail.com>
2023-08-09 15:54:51 -0500

    NavLinks for Cities and Countries written and working

--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,7 +1,22 @@
+import { NavLink } from "react-router-dom";
 import styles from "./AppNav.module.css";
 
 function AppNav() {
-  return <nav className={styles.nav}>App Navigation</nav>;
+  return (
+    <nav className={styles.nav}>
+      <ul>
+        <li>
+          <NavLink to="cities">Cities</NavLink>
+        </li>
+        <li>
+          <NavLink to="countries">Countries</NavLink>
+        </li>
+        {/* <li>
+          <NavLink to="form">Form</NavLink>
+        </li> */}
+      </ul>
+    </nav>
+  );
 }
 
 export default AppNav;
